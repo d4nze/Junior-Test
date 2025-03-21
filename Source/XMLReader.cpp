737d01e87318474f2d3926186d2cc273e8df858b
d4nze <daniiiiila123@gmail.com>
2025-03-21 11:17:07 +0300

    Added empty constructor.

--- Source/XMLReader.cpp
+++ Source/XMLReader.cpp
@@ -1,2 +1,4 @@
 #include "XMLReader.hpp"
 #include <MsXml6.h>
+
+XMLReader::XMLReader(std::string_view path) {}
--- Source/XMLReader.hpp
+++ Source/XMLReader.hpp
@@ -1,7 +1,11 @@
 #ifndef XML_READER_HPP
 #define XML_READER_HPP
+#include <string_view>
 
 class XMLReader
-{};
+{
+public:
+    XMLReader(std::string_view path);
+};
 
 #endif
