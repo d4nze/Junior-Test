5d989ee4f6766d6ad704fdedbc8853f6cd638121
d4nze <daniiiiila123@gmail.com>
2025-03-23 08:38:06 +0300

    Reworked Predicate class.

--- Source/Predicate.cpp
+++ Source/Predicate.cpp
@@ -1,16 +1,25 @@
 #include "Predicate.hpp"
+#include <algorithm>
 
-Predicate::Predicate(char symbol)
-    : m_symbol(symbol)
-    , m_count(-1)
-{}
+Predicate::Predicate(std::initializer_list<char>&& symbol, std::int32_t requires)
+    : m_symbolLenght(symbol.size())
+    , m_symbol(new char[m_symbolLenght])
+    , m_requires(requires)
+{
+    std::copy(symbol.begin(), symbol.end(), m_symbol);
+}
 
-char Predicate::getSymbol() const
+std::size_t Predicate::getSymbolLenght() const
+{
+    return m_symbolLenght;
+}
+
+char* Predicate::getSymbol() const
 {
     return m_symbol;
 }
 
 std::int32_t Predicate::getCount() const
 {
-    return m_count;
+    return m_requires;
 }
--- Source/Predicate.hpp
+++ Source/Predicate.hpp
@@ -1,21 +1,25 @@
 #ifndef PREDICATE_HPP
 #define PREDICATE_HPP
 
-#include <cstdint>
+#include <initializer_list>
+#include <array>
 
 class Predicate
 {
 public:
-    Predicate(char symbol);
+    Predicate(std::initializer_list<char>&& symbol, std::int32_t requires);
     virtual ~Predicate() = default;
 
-    virtual bool check() const = 0;
-    char getSymbol() const;
+    virtual bool compare(std::int32_t compareTo) const = 0;
+
+    std::size_t getSymbolLenght() const;
+    char* getSymbol() const;
     std::int32_t getCount() const;
 
 private:
-    char m_symbol;
-    std::int32_t m_count;
+    std::size_t m_symbolLenght;
+    char* m_symbol;
+    std::int32_t m_requires;
 };
 
 #endif
