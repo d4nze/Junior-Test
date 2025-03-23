68eeefab6fc799e6fba4236330b50e153170202c
d4nze <daniiiiila123@gmail.com>
2025-03-23 13:51:01 +0300

    Created PredicateFactory.

---
+++ Source/PredicateFactory.cpp
@@ -0,0 +1,8 @@
+#include "PredicateFactory.hpp"
+
+Predicate* PredicateFactory::createPredicate(const SymbolHolder& symbolHolder,
+                                             std::int32_t requires,
+                                             const std::string& predicate)
+{
+    return nullptr;
+}
---
+++ Source/PredicateFactory.hpp
@@ -0,0 +1,14 @@
+#ifndef PREDICATE_FACTORY_HPP
+#define PREDICATE_FACTORY_HPP
+
+#include "Predicate.hpp"
+
+class PredicateFactory
+{
+public:
+    static Predicate* createPredicate(const SymbolHolder& symbolHolder,
+                                      std::int32_t requires,
+                                      const std::string& predicate);
+};
+
+#endif
